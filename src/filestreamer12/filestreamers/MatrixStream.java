075ba0c31a1a6590c159aef000617d86a5c4109f
Karol <pokomeda.karol@gmail.com>
2017-06-14 08:10:51 +0200

    Refactor MatrixStream interface to use Matrix interface instead of MatrixImpl concrete class

--- src/filestreamer12/filestreamers/MatrixStream.java
+++ src/filestreamer12/filestreamers/MatrixStream.java
@@ -3,6 +3,7 @@ package filestreamer12.filestreamers;
 import filestreamer12.utils.Matrix;
 
 public interface MatrixStream {
-	boolean saveFile(Matrix matrix, String savePath);
-	Matrix loadFile(String loadPath);
+    boolean saveFile(Matrix matrix, String savePath);
+
+    Matrix loadFile(String loadPath);
 }
